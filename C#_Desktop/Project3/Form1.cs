17b2d8028650084e03c344a09cbc69df0a63307f
Mohammad Alhamad <heromi687@gmail.com>
2025-09-22 11:07:30 -0800

    Add files via upload

---
+++ C#_Desktop/Project3/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+
+        private int Counter = 0;
+        private int Counter2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeNowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void timerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.ShowDialog();
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            if (Start_btn.Text == "Start")
+            {
+                timer1.Start();
+                Start_btn.Text = "Stop";
+            }
+            else
+            {
+                timer1.Stop();
+                Start_btn.Text = "Start";
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Counter++;
+
+            TimeSpan ts = TimeSpan.FromSeconds(Counter);
+            label1.Text = ts.ToString(@"hh\:mm\:ss");
+
+        }
+
+        private void Restart_btn_Click(object sender, EventArgs e)
+        {
+            label1.Text = "00:00:00";
+            Counter = 0;
+        }
+
+        private void Mark_btn_Click(object sender, EventArgs e)
+        {
+
+            Counter2++;
+
+                ListViewItem item = new ListViewItem(Counter2.ToString());
+
+                item.SubItems.Add(DateTime.Now.ToString());
+                item.SubItems.Add(label1.Text);
+
+                listView1.Items.Add(item);
+      
+
+
+        }
+    }
+}
+
---
+++ C#_Desktop/Project3/Form3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form3 : Form
+    {
+        private int Counter = 0;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            int hours = (int)Count_hrs.Value;
+            int minutes = (int)Count_min.Value;
+            int seconds = (int)Count_sec.Value;
+
+            Counter = (hours * 3600) + (minutes * 60) + seconds;
+
+            TimeSpan ts = TimeSpan.FromSeconds(Counter);
+            label1.Text = ts.ToString(@"hh\:mm\:ss");
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Counter > 0)
+            {
+                Counter--;
+                TimeSpan ts = TimeSpan.FromSeconds(Counter);
+                label1.Text = ts.ToString(@"hh\:mm\:ss");
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Waking up time", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
