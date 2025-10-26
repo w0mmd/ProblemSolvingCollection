5c347ee317ea6f015b75148d710f7d4101bb2157
Mohammad Alhamad <heromi687@gmail.com>
2025-10-26 04:00:12 -0800

    Delete ContactsDataAccessLayer directory

--- ContactsDataAccessLayer/clsConnection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ContactsDataAccessLayer
-{
-     static class clsConnection
-    {
-        public static string ConnectionString ="Server=.;Database=ContactsDB;User Id=sa;Password=sa123456";
-    }
-}
